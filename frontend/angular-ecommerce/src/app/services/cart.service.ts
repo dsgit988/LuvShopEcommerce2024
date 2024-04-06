9058eba09fa11ff9a620f4dee7696993d61c38e9
Tom <pjoh1092@gmail.com>
2024-04-05 23:15:50 -0500

    Section 18 Shopping Cart Items CRUD

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -7,6 +7,7 @@ import { Subject } from 'rxjs';
 })
 export class CartService {
 
+
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>;
@@ -79,6 +80,28 @@ export class CartService {
     console.log("---------");
     
     
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if(theCartItem.quantity === 0){
+      this.remove(theCartItem);
+    }else{
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    //get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id);
+
+    //if found, remove the item from the array at the give index
+    if (itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
   }
 
 }
