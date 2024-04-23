d26cfd0030cf6f0b340ae7b3ad943672cdad50a7
Tom <pjoh1092@gmail.com>
2024-04-23 13:36:49 -0500

    Security Login Logout

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oagmqhdwenIytK8y5d7',
+        issuer: 'https://dev-14620741.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
