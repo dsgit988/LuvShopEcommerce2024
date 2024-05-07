af7785cef7391c2aaf8c19826c5761370d6b36de
Tom <pjoh1092@gmail.com>
2024-05-07 17:27:22 -0500

    Section 32 Secure Communication with HTTPS-Angular Frontend

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oagmqhdwenIytK8y5d7',
         issuer: 'https://dev-14620741.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
